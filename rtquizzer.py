47965e28880b7e901465f244e5799aef217c2df7
Fulgen301 <tokmajigeorge@gmail.com>
2017-11-02 15:50:37 +0100

    Commit

--- rtquizzer.py
+++ rtquizzer.py
@@ -4,17 +4,88 @@ import re
 from asyncirc import irc
 from enum import IntEnum
 import json
-
-import supybot.ircutils as ircutils
 import asyncirc.plugins.addressed
-import threading, time, os
+import threading, time, os, re
 import random
 import sys
+
+###
+#supybot.ircutils (https://github.com/ProgVal/limnoria/tree/master/src/ircutils.py)
+###
+
+class ircutils(object):
+    def bold(s):
+        """Returns the string s, bolded."""
+        return '\x02%s\x02' % s
+    
+    def italic(s):
+        """Returns the string s, italicised."""
+        return '\x1D%s\x1D' % s
+    
+    # Definition of mircColors dictionary moved below because it became an IrcDict.
+    def mircColor(s, fg=None, bg=None):
+        """Returns s with the appropriate mIRC color codes applied."""
+        if fg is None and bg is None:
+            return s
+        elif bg is None:
+            if str(fg) in mircColors:
+                fg = mircColors[str(fg)]
+            elif len(str(fg)) > 1:
+                fg = mircColors[str(fg)[:-1]]
+            else:
+                # Should not happen
+                pass
+            return '\x03%s%s\x03' % (fg.zfill(2), s)
+        elif fg is None:
+            bg = mircColors[str(bg)]
+            # According to the mirc color doc, a fg color MUST be specified if a
+            # background color is specified.  So, we'll specify 00 (white) if the
+            # user doesn't specify one.
+            return '\x0300,%s%s\x03' % (bg.zfill(2), s)
+        else:
+            fg = mircColors[str(fg)]
+            bg = mircColors[str(bg)]
+            # No need to zfill fg because the comma delimits.
+            return '\x03%s,%s%s\x03' % (fg, bg.zfill(2), s)
+    
+    def stripColor(s):
+        """Returns the string s, with color removed."""
+        return _stripColorRe.sub('', s)
+
+_stripColorRe = re.compile(r'\x03(?:\d{1,2},\d{1,2}|\d{1,2}|,\d{1,2}|)')
+mircColors = {
+    'white': '0',
+    'black': '1',
+    'blue': '2',
+    'green': '3',
+    'red': '4',
+    'brown': '5',
+    'purple': '6',
+    'orange': '7',
+    'yellow': '8',
+    'light green': '9',
+    'teal': '10',
+    'light blue': '11',
+    'dark blue': '12',
+    'pink': '13',
+    'dark grey': '14',
+    'light grey': '15',
+    'dark gray': '14',
+    'light gray': '15',
+}
+
+# We'll map integers to their string form so mircColor is simpler.
+for (k, v) in list(mircColors.items()):
+    if k is not None: # Ignore empty string for None.
+        sv = str(v)
+        mircColors[sv] = sv
+        mircColors[sv.zfill(2)] = sv
 
 class State(IntEnum):
     Question = 0
     Tips = 1
-    Answer = 2
+    Pause = 2
+    Answer = 3
 
 class Quizbot(object):
     quiz = None
@@ -27,6 +98,7 @@ class Quizbot(object):
     points = {}
     bot = None
     channel = "#rt-quiz"
+    counter = 0
     
     def __init__(self, bot):
         self.bot = bot
@@ -62,6 +134,7 @@ class Quizbot(object):
                 self.loadQuestions()
                 self.winner = None
                 self.tips = 1
+                self.counter = 0
                 self.current_category = random.choice(list(self.questions.keys()))
                 self.current_question = random.choice(self.questions[self.current_category])[:]
                 text = [f"Kategorie {ircutils.bold(self.current_category)}: ", ircutils.mircColor(self.current_question[0], 11, 2)]
@@ -83,13 +156,32 @@ class Quizbot(object):
                 continue
         
             elif self.mode == State.Tips:
+                if self.counter < 4:
+                    self.counter += 1
+                    time.sleep(5)
+                    continue
+                
                 self.reply("{}{}{}".format(ircutils.bold("Tipp: "), self.current_question[2][:self.tips], "." * (len(self.current_question[2]) - self.tips)))
                 self.tips += 1
                 if self.tips == len(self.current_question[2]):
-                    self.mode = State.Answer
+                    self.counter = 0
+                    self.mode = State.Pause
                 
                 time.sleep(4)
                 continue
+            
+            elif self.mode == State.Pause:
+                if not self.counter:
+                    self.reply(ircutils.mircColor("Achtung, wenn die Frage innerhalb von 60 Sekunden nicht beantwortet wird, werde ich automatisch eine neue Runde starten!", 3, 1))
+                
+                if self.counter < 12:
+                    self.counter += 1
+                    time.sleep(5)
+                    continue
+                else:
+                    self.counter = 0
+                    self.mode = State.Answer
+            
             elif self.mode == State.Answer:
                 if self.winner is not None:
                     if self.winner not in self.points:
@@ -120,7 +212,7 @@ quiz = None
 
 asyncirc.plugins.addressed.register_command_character("!")
 bot = irc.connect("irc.euirc.net", 6667, use_ssl=False)
-bot.register("RT-Quizzer", "RT-Quizzer", "RT-Quizzer").join([Quizbot.channel])
+bot.register("RT-Quizzer", "RT-Quizzer", "RT-Quizzer").join([Quizbot.channel, "#atlantis"])
 
 @bot.on("irc-001")
 def connected(par=None):
@@ -131,19 +223,57 @@ def on_addressed(message, user, target, text):
     global quiz
     
     if text == "punkte" and quiz:
-        for i, key in enumerate(sorted(quiz.points), start=1):
-            quiz.reply(f"{i}.\t{key} ({quiz.points[key]})")
-            
+        for i, p in enumerate(sorted(quiz.points.items(), key=lambda x: x[1], reverse=True), start=1):
+            quiz.reply(f"{i}.\t{p[0]} ({p[1]})")
             if i > 5:
                 break
+
+current_category = ""
+current_question = []
+questions = {}
 
 @bot.on("message")
 def on_message(message, user, target, text):
     if target == Quizbot.channel and quiz and quiz.current_question and not quiz.winner and text.lower() == quiz.current_question[2].lower():
         quiz.winner = user.nick
         quiz.mode = State.Answer
+    
+    elif target == "#atlantis" :
+        if user.nick != "Colin":
+            return
+    
+        global current_category, current_question, questions
+        
+        x = re.match(r"(.*): (.*\?)", text)
+        if x:
+            current_question = [ircutils.stripColor(x[2]), "", "", 0]
+            current_category = ircutils.stripColor(x[1])
+        
+        if not (current_category and current_question):
+            return
+        
+        x = re.match(r"Tipp : .* : (\d*) Punkte", text)
+        if x:
+            current_question[3] = x[1] // 200
+        
+        x = re.match(r".*-> (.*) <-.*", text)
+        if x:
+            current_question[2] = ircutils.stripColor(x[1])
+            if current_category not in questions:
+                questions[current_category] = [current_question[:]]
+            elif current_question not in questions[current_category]:
+                questions[current_category].append(current_question[:])
+            
+            current_question = []
+            current_category = ""
+            with open("questions.json", "r") as fobj:
+                old = json.load(fobj)
+            
+            old.update(questions)
+            with open("questions.json", "w") as fobj:
+                json.dump(old, fobj)
 
 @bot.on("connection-lost")
 def on_disconnected(*args):
-    sys.exit(2)
+    time.sleep(10)
 asyncio.get_event_loop().run_forever()
